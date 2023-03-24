4b3e59c987136a1ca23e1d7c35a916c34368e725
Kafilat <kafilatadewumi61@gmail.com>
2023-03-24 11:59:40 +0100

    temperature converter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [temp, setTemp] = useState({
+    f: 0,
+    c: 0 })
+
+const updateC = ev => setTemp({
+  c: ev.target.value,
+  f: (+ev.target.value * 9 / 5 + 32).toFixed(2)
+})
+
+const updateF = ev => setTemp({
+  c: ((+ev.target.value - 32) * 5 / 9).toFixed(2),
+  f: ev.target.value
+})
+  return (
+    <div className='wrapper'>
+        <div className="celsius-box">
+          <h1>Temperature in Celsius</h1>
+          <input type="number" value={temp.c} onChange={updateC} />
+        </div>
+      <div>
+        <h1>Temperature in Farenheit</h1>
+        <input type="number" value={temp.f} onChange={updateF} />
+      </div>
+    </div>
+  );
 }
 
 export default App
